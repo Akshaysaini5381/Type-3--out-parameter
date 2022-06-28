ddabbacf011c45a82a0cbcfe93a43f10e8d33864
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-29 04:16:56 +0530

    Add project files.

---
+++ Type 3- out parameter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Type_3__out_parameter
+{
+    class Program
+    {
+        static void show(out int x,out int y)
+        {
+            
+            Console.Write("Enter the value :");
+            int num = int.Parse(Console.ReadLine());
+            x = num * num;
+            y = num * num * num;
+            Console.WriteLine("first :"+x+" "+y);
+        }
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            show( out a,out b);
+            Console.WriteLine("Last :"+a+" "+b);
+        }
+    }
+}
